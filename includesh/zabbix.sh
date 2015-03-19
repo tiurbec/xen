51034278ac1f6fcb3e1b05a6c3a3da5926995012
Cristi Tiurbe <cristi@tiurbe.ro>
2015-03-19 12:50:40 +0200

    Zabbix agent restart updated

--- includesh/zabbix.sh
+++ includesh/zabbix.sh
@@ -5,6 +5,92 @@
 . ./config.sh
 . ./os.sh
 
+function updateZAconfig2 ()
+{
+IP=${1:-"127.0.0.1"}
+PORT=${2:-"22"}
+USER=${3:-"root"}
+IFILE=${4:-"/root/.ssh/id_rsa"}
+IDH=${5:-"-1"}
+SISNAME=${6:-"unknown"}
+NEWNAME=${7:-"empty"}
+SSHPARAMS=" -p $PORT $USER@$IP -i $IFILE "
+SCPPARAMS=${4:+" -i $IFILE "}
+ZACFGFILE="/etc/zabbix/zabbix_agentd.conf"
+ZAPIDFILE="/var/run/zabbix/zabbix_agentd.pid"
+ZALOGFILE="/var/log/zabbix/zabbix_agentd.log"
+ZAINCLUDE="/etc/zabbix/zabbix_agentd.d/*"
+ZAUSERDIR="/etc/zabbix/zabbix_agentd.d/"
+ROLES="expert"
+if [ $(hasZabbixAgent $IP $PORT $USER $IFILE) -eq 1 ];
+then
+  if [ $(isOpensde $IP $PORT $USER $IFILE) -eq 1 ];
+  then
+    ZACFGFILE="/home/zabbix/conf/zabbix_agentd.conf"
+    ZAPIDFILE="/home/zabbix/zabbix_agentd.pid"
+    ZALOGFILE="/home/zabbix/zabbix_agentd.log"
+    ZAINCLUDE="/home/zabbix/conf/zabbix_agentd/*"
+    ZAUSERDIR="/home/zabbix/conf/zabbix_agentd/"
+  fi
+  echo -n "hP"
+  if [ $(hasPostgres $IP $PORT $USER $IFILE) -eq 1 ];
+  then
+    ROLES="$ROLES,postgres"
+  fi
+  echo -n "hp"
+  if [ $(hasPhp $IP $PORT $USER $IFILE) -eq 1 ];
+  then
+    ROLES="$ROLES,php"
+  fi
+  echo -n "hn"
+  if [ $(hasNginx $IP $PORT $USER $IFILE) -eq 1 ];
+  then
+    ROLES="$ROLES,nginx"
+  fi
+  if [ $(isDom0 $IP $PORT $USER $IFILE) -eq 0 ];
+  then
+    ROLES="$ROLES,VirtualHost"
+    echo -n "vh"
+  else
+    ROLES="$ROLES,Dom0"
+    echo -n "d0"
+  fi
+  if [ "$NEWNAME" == "empty" ];
+  then
+    ZACFGHNAME=""
+  else
+    ZACFGHNAME="Hostname=$NEWNAME"
+  fi
+  echo -n "f"
+  ssh $SSHPARAMS $SSHOPTS "cat <<EOF > $ZACFGFILE
+PidFile=$ZAPIDFILE
+LogFile=$ZALOGFILE
+LogFileSize=0
+DebugLevel=2
+#SourceIP=$SOURCEIP
+$ZACFGHNAME
+ServerActive=144.76.106.136
+HostnameItem=system.hostname
+HostMetadata=$ROLES
+RefreshActiveChecks=120
+StartAgents=0
+Include=$ZAINCLUDE
+UserParameter=expert.address,curl -s http://144.76.106.136:800
+UserParameter=expert.sshport,echo \"$PORT\"
+UserParameter=expert.sisname,echo \"$SISNAME\"
+UserParameter=expert.sisidh,echo \"$IDH\"
+UserParameter=expert.hostname,hostname
+UserParameter=expert.osname,if test -f /etc/redhat-release; then cat /etc/redhat-release; else if test -f /etc/SDE-VERSION; then cat /etc/SDE-VERSION; else echo \"Unknown\";fi ;fi
+EOF" </dev/null
+#scp
+echo -n "scp"
+scp -P $PORT $SCPPARAMS $ZAFILES/etc/* $USER@$IP:$ZAUSERDIR >/dev/null 2>/dev/null
+scp -P $PORT $SCPPARAMS $ZAFILES/usr/* $USER@$IP:/usr/local/bin/ >/dev/null 2>/dev/null
+echo -n "R"
+restartZabbixAgent $IP $PORT $USER $IFILE
+fi
+}
+
 function updateZAconfig ()
 {
 IP=${1:-"127.0.0.1"}
@@ -15,10 +101,12 @@ IDH=${5:-"-1"}
 SISNAME=${6:-"unknown"}
 NEWNAME=${7:-"empty"}
 SSHPARAMS=" -p $PORT $USER@$IP -i $IFILE "
+SCPPARAMS=${4:+" -i $IFILE "}
 ZACFGFILE="/etc/zabbix/zabbix_agentd.conf"
 ZAPIDFILE="/var/run/zabbix/zabbix_agentd.pid"
 ZALOGFILE="/var/log/zabbix/zabbix_agentd.log"
 ZAINCLUDE="/etc/zabbix/zabbix_agentd.d/*"
+ZAUSERDIR="/etc/zabbix/zabbix_agentd.d/"
 ROLES="expert"
 if [ $(hasZabbixAgent $IP $PORT $USER $IFILE) -eq 1 ];
 then
@@ -28,6 +116,7 @@ then
     ZAPIDFILE="/home/zabbix/zabbix_agentd.pid"
     ZALOGFILE="/home/zabbix/zabbix_agentd.log"
     ZAINCLUDE="/home/zabbix/conf/zabbix_agentd/*"
+    ZAUSERDIR="/home/zabbix/conf/zabbix_agentd/"
   fi
   if [ $(hasPostgres $IP $PORT $USER $IFILE) -eq 1 ];
   then
@@ -72,16 +161,14 @@ UserParameter=expert.sisname,echo \"$SISNAME\"
 UserParameter=expert.sisidh,echo \"$IDH\"
 UserParameter=expert.hostname,hostname
 UserParameter=expert.osname,if test -f /etc/redhat-release; then cat /etc/redhat-release; else if test -f /etc/SDE-VERSION; then cat /etc/SDE-VERSION; else echo \"Unknown\";fi ;fi
-UserParameter=expert.consoleusers,who -u |wc -l
-UserParameter=expert.psqlpcount,ps ax|grep postgres|grep -v grep|wc -l
-UserParameter=expert.load1,cat /proc/loadavg | cut -d\  -f1
-UserParameter=expert.load5,cat /proc/loadavg | cut -d\  -f2
-UserParameter=expert.load10,cat /proc/loadavg | cut -d\  -f3
 EOF" </dev/null
+#scp
+scp -P $PORT $SCPPARAMS $ZAFILES/etc/* $USER@$IP:$ZAUSERDIR >/dev/null 2>/dev/null
+scp -P $PORT $SCPPARAMS $ZAFILES/usr/* $USER@$IP:/usr/local/bin/ >/dev/null 2>/dev/null
+
 restartZabbixAgent $IP $PORT $USER $IFILE
 fi
 }
-
 function putZabbixAgent ()
 {
 IP=${1:-"127.0.0.1"}
@@ -190,14 +277,19 @@ PORT=${2:-"22"}
 USER=${3:-"root"}
 IFILE=${4:-"/root/.ssh/id_rsa"}
 SSHPARAMS=" -p $PORT $USER@$IP -i $IFILE "
-RETSTR=$(ssh $SSHPARAMS $SSHOPTS '/etc/init.d/zabbix_agentd stop >/dev/null 2>/dev/null' </dev/null)
-RETSTR=$(ssh $SSHPARAMS $SSHOPTS '/etc/init.d/zabbix_agentd start >/dev/null 2>/dev/null' </dev/null)
+if [ $(isOpensde $IP $PORT $USER $IFILE) -eq 1 ];then
+   RETSTR=$(ssh $SSHPARAMS $SSHOPTS '/etc/init.d/zabbix_agentd stop >/dev/null 2>/dev/null' </dev/null)
+   RETSTR=$(ssh $SSHPARAMS $SSHOPTS 'export PATH=$PATH:/opt/postgresql/bin;/etc/init.d/zabbix_agentd start >/dev/null 2>/dev/null' </dev/null)
+else
+   RETSTR=$(ssh $SSHPARAMS $SSHOPTS '/etc/init.d/zabbix-agent stop >/dev/null 2>/dev/null' </dev/null)
+   RETSTR=$(ssh $SSHPARAMS $SSHOPTS '/etc/init.d/zabbix-agent start >/dev/null 2>/dev/null' </dev/null)
+fi
 if [ $? -eq 0 ];
 then
-   echo "1"
+   echo -n "1"
    return 1
 fi
-echo "0"
+echo -n "0"
 return 0
 }
 
@@ -257,6 +349,15 @@ HostnameItem=system.hostname
 HostMetadata=$ROLES
 RefreshActiveChecks=120
 StartAgents=0
+Include=/etc/zabbix/zabbix_agentd.d/*
+UserParameter=expert.address,curl -s http://10.1.1.10
+UserParameter=expert.sshport,echo \"$PORT\"
+UserParameter=expert.one,echo \"1\"
+UserParameter=expert.sisname,echo \"\"
+UserParameter=expert.sisidh,echo \"8\"
+UserParameter=expert.hostname,hostname
+UserParameter=expert.osname,if test -f /etc/redhat-release; then cat /etc/redhat-release; else if test -f /etc/SDE-VERSION; then cat /etc/SDE-VERSION; else echo \"Unknown\";fi ;fi
+
 EOF" </dev/null
 
 scp -P $PORT $SCPPARAMS /root/git/xen/files/zabbix-agent/postgres/etc/* $USER@$IP:/etc/zabbix/zabbix_agentd.d/
@@ -313,6 +414,15 @@ HostnameItem=system.hostname
 HostMetadata=$ROLES
 RefreshActiveChecks=120
 StartAgents=0
+Include=/home/zabbix/conf/zabbix_agentd/*
+UserParameter=expert.address,curl -s http://10.1.1.10
+UserParameter=expert.sshport,echo \"$PORT\"
+UserParameter=expert.one,echo \"1\"
+UserParameter=expert.sisname,echo \"\"
+UserParameter=expert.sisidh,echo \"8\"
+UserParameter=expert.hostname,hostname
+UserParameter=expert.osname,if test -f /etc/redhat-release; then cat /etc/redhat-release; else if test -f /etc/SDE-VERSION; then cat /etc/SDE-VERSION; else echo \"Unknown\";fi ;fi
+
 EOF
 "</dev/null
 #cat <<'EOF' | ssh $SSHPARAMS $SSHOPTS "cat > /etc/init.d/zabbix_agentd" </dev/null
