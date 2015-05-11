d3efb3c478747c2dff6ce1d23f58bdbf24495497
Cristi Tiurbe <cristi@tiurbe.ro>
2015-05-11 11:37:10 +0300

    xen config files updated

--- xs_crxinst.sh
+++ xs_crxinst.sh
@@ -28,7 +28,7 @@ name = "$DUHOSTNAME"
 kernel = "/usr/local/xen/vmlinuz"
 ramdisk = "/usr/local/xen/initrd.img"
 extra = "root=/dev/xvda ip=$DUIP netmask=255.255.255.0 gateway=10.1.1.254 dns=8.8.8.8 text ks=http://144.76.106.136:800/$XKS"
-memory = 1024
+memory = 2048 
 maxmem = 2048
 vcpus = 2
 vif = [ 'mac=$DUMAC, bridge=xenbr0' ]
--- xs_crxrun.sh
+++ xs_crxrun.sh
@@ -25,8 +25,8 @@ cat <<EOF > ./$XRUN
 name = "$DUHOSTNAME"
 kernel = "/usr/lib/xen/boot/pv-grub-x86_64.gz"
 extra = "(hd0)/grub/menu.lst console=hvc0"
-memory = 1024
-maxmem = 1024
+memory = 2048
+maxmem = 2048
 vcpus = 2
 vif = [ 'mac=$DUMAC, bridge=xenbr0, vifname=vif.$DUHOSTNAME' ]
 disk = [ '/dev/$VGNAME/$DUHOSTNAME-root,raw,xvda3,rw', 
